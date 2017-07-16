83f31238ee3e05562f4f815e5b1b94b85526656c
William Thomas <wthoma390@gmail.com>
2017-07-15 22:52:46 -0600

    adds custom functionality to devise with fullname

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :fullname
+      devise_parameter_sanitizer.for(:account_update) << :fullname
+    end
 end
