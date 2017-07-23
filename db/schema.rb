8614c93b267a4e0748c8a99e99e288f23ec0a899
William Thomas <wthoma390@gmail.com>
2017-07-22 22:55:48 -0600

    creates user controller, and adds css to user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170723013734) do
+ActiveRecord::Schema.define(version: 20170723042602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20170723013734) do
     t.string "provider"
     t.string "uid"
     t.string "image"
+    t.string "phone_number"
+    t.text "description"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
