9518c424fdb814a75ee0e98970404e2d496e1caa
William Thomas <wthoma390@gmail.com>
2017-08-05 14:25:06 -0600

    addes added_attrs refactor in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      # added_attrs = [:fullname, :phone_number, :description, :email, :password]
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :phone_number, :description, :email, :password])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:fullname, :phone_number, :description, :email, :password])
+      added_attrs = [:fullname, :phone_number, :description, :email, :password]
+      devise_parameter_sanitizer.permit(:sign_up, keys: [added_attrs])
+      devise_parameter_sanitizer.permit(:account_update, keys: [added_attrs])
     end
 end
