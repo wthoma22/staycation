18f6feaf14ce020af79a7c9e78a64edd2db5af64
William Thomas <wthoma390@gmail.com>
2017-08-06 15:05:47 -0600

    Messaging   *run conversation and message migrations   *create models for conversation and message   *update relationships in models   *validate presence and uniqueness in models   *add scopes and methods in  models   *create conversation and messages controllers   *update routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806052400) do
+ActiveRecord::Schema.define(version: 20170806204301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "conversation_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "photos", force: :cascade do |t|
     t.bigint "room_id"
@@ -90,6 +107,8 @@ ActiveRecord::Schema.define(version: 20170806052400) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
   add_foreign_key "photos", "rooms"
   add_foreign_key "reservations", "rooms"
   add_foreign_key "reservations", "users"
---
+++ spec/factories/messages.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :message do
+    content "MyText"
+    conversation nil
+    user nil
+  end
+end
