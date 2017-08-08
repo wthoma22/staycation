cdbb14d9e8fd7997cb8148a03c5cd62eb50d36f2
William Thomas <wthoma390@gmail.com>
2017-08-08 12:52:04 -0600

    Paypal   *update create in reservation controller to include paypal information   *download ngrok for testing server   *add protect from forgery in reservation controller to notify and create   *add condition to your trip and your reservations so only completed transactions show   *update routes to include post methods for notify and your trips

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807001724) do
+ActiveRecord::Schema.define(version: 20170808181614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20170807001724) do
     t.integer "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "status"
     t.index ["room_id"], name: "index_reservations_on_room_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
